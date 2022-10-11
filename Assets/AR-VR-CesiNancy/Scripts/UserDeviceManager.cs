724b9cdfd541ed9bd89abf5c22285e82b09e9ad1
Clement <clement.bigot@viacesi.fr>
2022-10-11 21:00:25 +0200

    NetworkManager start

---
+++ Assets/AR-VR-CesiNancy/Scripts/UserDeviceManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UserDeviceType
+{
+    HTC,
+    PC
+}
+
+public class UserDeviceManager : MonoBehaviour
+{
+    
+}
